5f7929ebf599f7511a7053edbd569676bd883745
BASimpson <160790538+BASimpson@users.noreply.github.com>
2024-03-13 14:22:28 -0500

    Added Pkmn_Class_Settings.cpp Modified Pokemon_Team to start creating header file stuff

---
+++ CSV reader/Pkmn_Class_Settings.cpp
@@ -0,0 +1,1 @@
+//Class settings file for the Pokemon class
--- CSV reader/Pokemon_Main.cpp
+++ CSV reader/Pokemon_Main.cpp
@@ -1,11 +1,13 @@
 // CSV reader.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include "Pokemon_Team.h"
 int main()
 {
-    std::cout << "Hello Worldabc!\n";
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
