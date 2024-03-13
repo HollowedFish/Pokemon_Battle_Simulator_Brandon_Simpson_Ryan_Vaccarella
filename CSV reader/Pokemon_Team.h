f5cad776cfb87f221859d7ac6a472aace5fc401c
BASimpson <160790538+BASimpson@users.noreply.github.com>
2024-03-13 14:41:19 -0500

    Lets try this for a third time

--- CSV reader/Pokemon_Team.h
+++ CSV reader/Pokemon_Team.h
@@ -1,1 +1,29 @@
 #pragma once
+//Header file for the Pokemon class
+#pragma once
+#include <string>
+#include <fstream>
+//Functions are in camel case
+class Pokemon {
+private:
+	int atk;
+	int def;
+	int spAtk;
+	int spDef;
+	int hp;
+	int spd;
+	std::string name;
+	std::string type1;
+	std::string type2;
+
+	//create an array for moves somewhere
+
+public:
+	Pokemon() {}
+	Pokemon(std::string pkmn) {}
+	void setAtk(int atk) {}
+	void setDef(int def) {}
+	void setSpAtk(int spDef) {}
+
+
+};
