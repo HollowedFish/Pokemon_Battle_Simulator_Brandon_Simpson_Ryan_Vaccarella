8e878e00168c5f5e0d96dc9dcd58b713b74c47b6
BASimpson <160790538+BASimpson@users.noreply.github.com>
2024-03-13 15:23:44 -0500

    Added a settings file for the Pokemon class & added to class

---
+++ CSV reader/Pokemon_Class_Settings.cpp
@@ -0,0 +1,5 @@
+//settings for the pokemon class
+#include "Pokemon_Team.h"
+#include <string>
+#include <fstream>
+#include <iostream>
--- CSV reader/Pokemon_Team.h
+++ CSV reader/Pokemon_Team.h
@@ -2,7 +2,6 @@
 //Header file for the Pokemon class
 #pragma once
 #include <string>
-#include <fstream>
 //Functions are in camel case
 class Pokemon {
 private:
@@ -15,15 +14,32 @@ private:
 	std::string name;
 	std::string type1;
 	std::string type2;
+	bool KO;
 
 	//create an array for moves somewhere
 
 public:
 	Pokemon() {}
 	Pokemon(std::string pkmn) {}
+	//Setter functions
 	void setAtk(int atk) {}
 	void setDef(int def) {}
 	void setSpAtk(int spDef) {}
-
-
+	void setHp(int hp) {}
+	void setSpd(int spd) {}
+	void setName(std::string name) {}
+	void setType1(std::string type1) {}
+	void setType2(std::string type2) {}
+	//Getter functions
+	int getAtk() {}
+	int getDef() {}
+	int getSpAtk() {}
+	int getSpDef() {}
+	int getSpd() {}
+	std::string getName() {}
+	std::string getType1() {}
+	std::string getType2() {}
+	//Misc functions
+	bool isKO() {}
+	void setKO() {}
 };
